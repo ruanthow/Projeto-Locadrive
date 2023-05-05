d3a8fe1ec206373c32dcaba6081b9a4b7b99ec25
ruanthow <ruan.thow3@gmail.com>
2023-05-05 14:49:58 -0300

    Alterando responsividade para mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 let btnLogin = document.getElementById("login");
+var navMobile = document.querySelector(".nav-mobile-menu");
 
 function openModal(){
    console.log("abriu");
+}
+
+function closeMenu(){
+   navMobile.style.width = "0px"
+}
+
+function showMenu(){
+   navMobile.style.width = "100%"
 }
