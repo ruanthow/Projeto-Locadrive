eaa2323544d58424dbe39f57aa79b329020e3642
eliasmunizrj <108908212+eliasmunizrj@users.noreply.github.com>
2023-05-21 04:13:52 -0300

    Ajustando css e adicionando Perguntas Frequentes e mudando o footer, botao do whatsapp adicionado no canto inferior direito

--- src/View/js/index.js
+++ src/View/js/index.js
@@ -12,3 +12,28 @@ function closeMenu(){
 function showMenu(){
    navMobile.style.width = "100%"
 }
+
+/*SCROLL SUAVE */
+
+window.scroll({
+   top:0,
+   behavior: 'smooth',
+})
+
+/* ACORDEAO */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
