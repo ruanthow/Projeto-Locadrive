d840081fe9d240558f0376057011bfc0869e6d21
ruanthow <ruan.thow3@gmail.com>
2023-05-23 23:12:33 -0300

    Em andamento com pagina de gerenciamento de cliente

--- src/components/htmlCliente.js
+++ src/components/htmlCliente.js
@@ -1,6 +1,9 @@
-let renderConteudo = document.querySelector(".renderConteudo");
+import {paramsCampos} from "http://localhost/Projeto-Locadrive/src/components/validacao.js";
 
-function htmlCliente(){
+let renderConteudo = document.querySelector(".renderConteudo");
+let teste = document.querySelector("#teste");
+
+window.htmlListar = function (){
     return (
         renderConteudo.innerHTML =
         `
@@ -50,3 +53,127 @@ function htmlCliente(){
         `
     )
 }
+
+
+
+window.htmlEditar = function (){
+    renderConteudo.innerHTML = 
+    `
+    <div class='col-12' id="formSearch">
+        <div class='menuPesquisa d-flex align-items-center'>
+            <h4>Clientes</h4>
+            <div class="menuPesquisaInput">
+                <input class='form-input' type='text' id="inputSearch" placeholder="pesquisar"/>
+                <button type='button' id="btnSearch" onclick="getAllUsers()">
+                    <div>
+                        <img src="./assets/iconSend.svg" alt="">
+                    </div>
+                </button>
+            </div>
+            <div class='col-2'>
+                <select id="typeData" class='menuPesquisaSelect form-select' aria-label='Default select example'>
+                    <option value='id'>id</option>
+                    <option value='nome'>nome</option>
+                    <option value='email'>email</option>
+                </select>
+            </div>
+        </div>
+    </div>    
+    <div class="content">
+      <form method="post" class="col-7">
+        <input type="hidden" name="cad" value="cadastrar">
+        <div class="form-row">
+          <div class="col-md-6 mb-3">
+            <label for="validationDefault01">Nome</label>
+            <input type="text" onchange="validNameSobrenome()" name="nome" id="nome" class="form-control required"
+              placeholder="Nome" required>
+            <span class="validacao">Não ultilize números ou caracteres especias</span>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="validationDefault02">Sobrenome</label>
+            <input type="text" onchange="validNameSobrenome()" name="sobrenome" class="form-control required"
+              id="sobrenome" placeholder="Sobrenome" value="" required>
+            <span class="validacao">Não ultilize números ou caracteres especias</span>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="validationDefaultUsername">Usuário</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroupPrepend2">@</span>
+              </div>
+              <input type="text" class="form-control required" id="usuario" placeholder="Usuário"
+                aria-describedby="inputGroupPrepend2" required>
+            </div>
+            <span class="validacao">Digite um usuario válido</span>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="validationDefaultPassword">Senha</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroupPrepend1">*</span>
+              </div>
+              <input type="password" onchange="validSenha()" name="senha" class="form-control required" id="senha"
+                placeholder="Senha" aria-describedby="inputGroupPrepend1" required>
+            </div>
+            <span class="validacao">Minimo de 8 caracteres</span>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="col-md-6 mb-3">
+            <label for="validationDefault03">Cidade</label>
+            <input type="text" onchange="validEstadoCidade()" name="cidade" class="form-control required" id="cidade"
+              placeholder="Cidade" required>
+            <span class="validacao">Digite o nome da sua cidade</span>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label for="validationDefault04">Estado</label>
+            <input type="text" onchange="validEstadoCidade()" name="estado" class="form-control required" id="estado"
+              placeholder="Estado" required>
+            <span class="validacao">Digite o nome da sua Estado</span>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label for="validationDefault05">CEP</label>
+            <input type="text" onchange="validCep()" name="cep" class="form-control required" id="cep" placeholder="CEP"
+              required>
+            <span class="validacao">Digite seu cep</span>
+          </div>
+          <div class="col-md-4 mb-3">
+            <label for="validationDefault06"> Telefone</label>
+            <input type="text" onchange="validTelefone()" name="telefone" class="form-control required" id="telefone"
+              placeholder="Telefone" required>
+            <span class="validacao">Digite seu telefone</span>
+          </div>
+          <div class="col-md-5 mb-3">
+            <label for="validationDefault07">E-mail</label>
+            <input type="text" onchange="validEmail()" name="email" class="form-control required" id="email"
+              placeholder="E-mail" required>
+            <span class="validacao">Digite um email válido</span>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label for="validationDefault08">Data de Nascimento</label>
+            <input type="date" onchange="validData()" name="data" class="form-control required" id="data"
+              placeholder="Aniversário" required>
+            <span class="validacao">Deve ser maior de idade</span>
+            <span class="validacaoData">Data invalida</span>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-check col-md-6 mb-3">
+            <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+            <label class="form-check-label" for="invalidCheck2">
+              Concordo com os termos e condições
+            </label>
+          </div>
+          <img class="img-car"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/96fdb578684029.5cac4a000cece.gif" alt="">
+        </div>
+        <div class="d-flex justify-content-center my-2">
+          <span class="validacao">Preencha todos os campos corretamente</span>
+        </div>
+        <button class="btn-primary" type="button" onclick="enviar()">Enviar</button>
+      </form>
+    </div>
+    `
+    paramsCampos(".required", ".validacao", ".validacaoData");
+}
+
---
+++ src/components/validacao.js
@@ -0,0 +1,249 @@
+import {receberValores} from 'http://localhost/Projeto-Locadrive/src/View/js/register.js';
+// let campos = document.querySelectorAll(".required"); 
+// let msgErro = document.querySelectorAll(".validacao"); 
+// let msgErroData = document.querySelector(".validacaoData"); 
+
+//em gerenciarCliente o js executa mesmo se o html do resgisto nao esta lá então essa funcção executarar quando renderizar os campos pegandos os inputs
+
+// campos filtrados para enviar a requisição
+let nomeValid = "";
+let sobrenomeValid = "";
+let usuarioValid = "";
+let senhaValid = "";
+let cidadeValid = "";
+let estadoValid = "";
+let cepValid = "";
+let telefoneValid = 0;
+let emailValid = "";
+let dataValid = "";
+
+
+let campos;// pegar todos os campos do form
+let msgErro;// pegar todos os spans com msg de erro
+let msgErroData;// span unico para erro de compatibilidade do compo data
+
+export function paramsCampos(classInputsFormulario,spanErros,spanErroData){
+    campos = document.querySelectorAll(classInputsFormulario);
+    msgErro = document.querySelectorAll(spanErros);
+    msgErroData = document.querySelector(spanErroData);
+}
+
+
+//Tornar msg de erro de campo especifico visivel
+function setError(num, num2) {
+    if (num2 == undefined) {
+        campos[num].style.border = "1px solid red";
+        msgErro[num].style.display = "block";
+    }
+    else {
+        campos[num].style.border = "1px solid red";
+        msgErro[num].style.display = "block";
+        campos[num2].style.border = "1px solid red";
+        msgErro[num2].style.display = "block";
+    }
+}
+//Tornar msg de erro de compatibilidade de campo data visivel
+function setErrorData() {
+    campos[9].style.border = "1px solid red";
+    msgErroData.style.display = "block";
+}
+
+//Tornar msg de erro de compatibilidade de campo data invisivel
+function setValidData() {
+    campos[9].style.border = "1px solid #ced4da";
+    msgErroData.style.display = "none";
+}
+
+//Tornar msg de erro de campo especifico invisivel
+function setValid(num, num2) {
+    if (num2 == undefined) {
+        campos[num].style.border = "1px solid #ced4da";
+        msgErro[num].style.display = "none";
+    }
+    else {
+        campos[num].style.border = "1px solid #ced4da";
+        msgErro[num].style.display = "none";
+        campos[num2].style.border = "1px solid #ced4da";
+        msgErro[num2].style.display = "none";
+    }
+
+}
+
+
+// validaçao de caracteres para campo nome e sobrenome
+window.validNameSobrenome = function () {
+    let nomeRegex = new RegExp(/(^[a-zA-Zà-úÀ-Ú ]*$)/gm);
+    let sobrenomeRegex = new RegExp(/(^[a-zA-Zà-úÀ-Ú ]*$)/gm);
+    let campoNome = nomeRegex.test(campos[0].value);
+    let campoSobrenome = sobrenomeRegex.test(campos[1].value);
+
+
+    if (campoNome && campoSobrenome) {
+        setValid(0, 1);
+        nomeValid = campos[0].value;
+        sobrenomeValid = campos[1].value;
+    }
+    else if (!campoNome && campoSobrenome) {
+        setError(0);
+        setValid(1);
+    }
+    else if (campoNome && !campoSobrenome) {
+        setError(1);
+        setValid(0);
+    }
+    else {
+        setError(0, 1);
+
+    }
+}
+
+
+window.validSenha = function () {
+    if (campos[3].value.length < 8) {
+        setError(3)
+    }
+    else {
+        setValid(3)
+        senhaValid = campos[3].value;
+    }
+}
+
+// validaçao de caracteres para campo estado e cidade
+window.validEstadoCidade = function () {
+    let cidadeRegex = new RegExp(/(^[a-zA-Zà-úÀ-Ú ]*$)/gm);
+    let estadoRegex = new RegExp(/(^[a-zA-Zà-úÀ-Ú ]*$)/gm);
+    let campoEstado = estadoRegex.test(campos[5].value);
+    let campoCidade = cidadeRegex.test(campos[4].value);
+
+    if (campoEstado && campoCidade) {
+        setValid(5, 4);
+        estadoValid = campos[5].value;
+        cidadeValid = campos[4].value;
+    }
+    else if (!campoEstado && campoCidade) {
+        setError(5);
+        setValid(4);
+    }
+    else if (campoEstado && !campoCidade) {
+        setError(4);
+        setValid(5);
+    }
+    else {
+        setError(5, 4);
+
+    }
+}
+
+//validar campo para cep valido
+window.validCep = function () {
+    let cep;
+    if (campos[6].value.length < 9 && campos[6].value.length > 7) {
+
+        let str = campos[6].value;
+        let inicio = str.slice(0, 5);
+        let final = str.slice(5);
+        cep = campos[6].value = inicio + "-" + final;
+    }
+
+    let Regex = new RegExp(/^[0-9]{5}[-][0-9]{3}$/gm);
+    let campoCep = Regex.test(cep);
+
+    if (campoCep) {
+        setValid(6);
+        cepValid = cep;
+    }
+    else {
+        setError(6);
+    }
+
+}
+
+// validar campo de telefone com apenas numeros
+window.validTelefone = function () {
+    let Regex = new RegExp(/^[0-9]{11}$/gm);
+    let campoTelefone = Regex.test(campos[7].value);
+
+    if (campoTelefone) {
+        setValid(7)
+        telefoneValid = campos[7].value;
+    }
+    else {
+        setError(7)
+    }
+}
+//validação de tipo de email
+window.validEmail = function () {
+    let Regex = new RegExp(/^([A-z0-9\._-])+@([A-z])+[.]+[A-z]{2,4}$/gm);
+    let campoEmail = Regex.test(campos[8].value);
+
+    if (campoEmail) {
+        setValid(8)
+        emailValid = campos[8].value;
+    }
+    else {
+        setError(8)
+    }
+}
+
+//validação de data 
+window.validData = function () {
+    let dataAgora = new Date();
+    let dataAgoraDia = dataAgora.getUTCDate();
+    let dataAgoraMes = (dataAgora.getMonth() + 1);
+    let dataAgoraAno = dataAgora.getFullYear();
+    let data = campos[9].value.split("-");
+    let Regex = new RegExp(/^([0-9]{4})+[-]+([0-9]{2})+[-]+([0-9]{2})$/gm);
+    let campoData = Regex.test(campos[9].value);
+
+    if (campoData) {
+        setValidData();
+        let calcAno = dataAgoraAno - data[0];
+        let calcMes = dataAgoraMes - data[1];
+        let calcDia = dataAgoraDia - data[2];
+
+        if (calcAno == 18 && calcMes >= 0 && calcDia >= 0) {
+            setValid(9)
+            dataValid = campos[9].value;
+        }
+        else if (calcAno > 18) {
+            setValid(9)
+            dataValid = campos[9].value;
+        }
+        else {
+            setError(9)
+        }
+    }
+    else {
+        setErrorData();
+    }
+}
+
+
+
+window.enviar =  function () {
+    usuarioValid = campos[2].value;
+    let dados = {
+
+        nomeValid: nomeValid,
+        sobrenomeValid: sobrenomeValid,
+        usuarioValid: usuarioValid,
+        senhaValid: senhaValid,
+        cidadeValid: cidadeValid,
+        estadoValid: estadoValid,
+        cepValid: cepValid,
+        telefoneValid: telefoneValid,
+        emailValid: emailValid,
+        dataValid: dataValid
+
+    }
+
+
+    if (nomeValid != "" && sobrenomeValid != "" && usuarioValid != "" && senhaValid != "" && cidadeValid != "" && estadoValid != "" && cepValid != "" && telefoneValid != 0 && emailValid != "" && dataValid != "") {
+        receberValores(dados);
+        msgErro[10].style.display = "none";
+    
+    }
+    else {
+        msgErro[10].style.display = "block";
+    }
+}
